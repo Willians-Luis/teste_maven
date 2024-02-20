44309bdce3be9aa7c72f810bf3ab868fdad05c81
Willians Luis Mataram <williansmataram@gmail.com>
2024-02-20 14:09:28 -0300

    first commit

---
+++ src/main/java/teste/teste/TesteApplication.java
@@ -0,0 +1,16 @@
+package teste.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TesteApplication.class, args);
+
+		System.out.println("teste funcionando");
+	}
+
+}
